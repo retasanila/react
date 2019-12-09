9c769ac392e22bcd69e30e7f26d36229d25b7b89
Reta Sanila <reta.sanila@gmail.com>
2019-12-09 14:04:18 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View
+} from 'react-native';
+
+import Routes from './src/Routes';
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <Routes/>
+      );
+    }
+  }
---
+++ src/pages/Item.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text, 
+  View,
+  Button,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+
+  AppRegistry,  
+  Platform
+} from 'react-native';
+
+import {Actions} from 'react-native-router-flux';
+
+export default class Thirdscreen extends Component<{}> {
+ 
+chat() {
+    Actions.firstscreen();
+  }
+
+  render() {
+    return (
+
+      <View style={styles.container}>
+        <View style={styles.image}>
+          <Image style={{width: 350, 
+            height: 350, 
+            borderRadius: 5,
+            marginVertical: 10}} 
+            source={require('../images/3.jpg')}/>
+        </View>
+        <View style={styles.tittle}>
+          <Text style={styles.text}>CHOCHO BOOK</Text>
+          <Text style={styles.pay}> Rp.100.000,00-</Text>
+        </View>
+      
+
+      <View style={styles.footer}>
+
+            <View style={styles.nav}>
+            <TouchableOpacity onPress={this.chat}>
+            <Image
+                style={{width: 60, height: 65}}
+                source={require('../images/chat.png')} />
+            </TouchableOpacity>
+            </View>
+
+            <View style={styles.nav}>
+            <TouchableOpacity>
+            <Image
+                style={{width: 60, height: 60}}
+                source={require('../images/trolley.png')} />
+            </TouchableOpacity>
+            </View>
+
+        </View>
+        </View>
+
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor:'#ffeeff', 
+    flex: 1,
+  },
+
+  image: {
+    alignItems: 'center',
+   },
+
+  text: {
+    fontSize: 30,
+  },
+
+  pay: {
+    fontSize: 20,
+  },
+
+  tittle: {
+    color: '#321911',
+    left: 5,
+    top: 0,
+    
+  },
+
+  nav:{
+    flex: 1, 
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f8bbd0'
+  },
+
+  footer:{
+    backgroundColor: '#f8bbd0',
+    flexDirection: 'row',
+    height: 50,  
+    borderTopWidth: 2,
+    borderTopColor: '#ddd',
+    marginVertical: 128
+  },
+
+
+});
